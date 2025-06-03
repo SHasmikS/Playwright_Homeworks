0566781bf5aa27d4c6a93ef552d56f624fadb85e
SHasmikS <hasmik.suqiasyan@gmail.com>
2025-06-03 11:48:10 +0400

    Updates

--- tests/homework2.spec.ts
+++ tests/homework2.spec.ts
@@ -2,9 +2,11 @@ import { test, expect } from '@playwright/test';
 
 //Successful sign in
 test('Successful sign in', async ({ page }) => {
+  const email = process.env.LOGIN_EMAIL!;
+  const password = process.env.LOGIN_PASSWORD!;
   await page.goto('https://playground.testingart.com/', { waitUntil: 'load' });
-  await page.getByRole('textbox', { name: 'Email Address' }).fill('testingart@email.com');
-  await page.getByRole('textbox', { name: 'Password' }).fill('Testing!123');
+  await page.getByRole('textbox', { name: 'Email Address' }).fill(email);
+  await page.getByRole('textbox', { name: 'Password' }).fill(password);
   await page.getByRole('button', { name: 'Sign In' }).click();
   await expect(page.getByText("Logout")).toBeVisible();
   await page.getByText("Logout").click();
@@ -24,8 +26,9 @@ test('Successful sign in', async ({ page }) => {
 
   //Valid email, empty password
   test('Valid email/empty password', async ({ page }) => {
+  const email = process.env.LOGIN_EMAIL!;
   await page.goto('https://playground.testingart.com/', { waitUntil: 'load' });
-  await page.getByRole('textbox', { name: 'Email Address' }).fill('testingart@email.com');
+  await page.getByRole('textbox', { name: 'Email Address' }).fill(email);
   await page.getByRole('textbox', { name: 'Password' }).click();
   await page.getByRole('button', { name: 'Sign In' }).click();
   await expect(page.getByText("Invalid Email or Password")).toBeVisible();
@@ -34,9 +37,10 @@ test('Successful sign in', async ({ page }) => {
 
   //Empty email, valid password
   test('Empty email, valid password', async ({ page }) => {
+  const password = process.env.LOGIN_PASSWORD!;
   await page.goto('https://playground.testingart.com/', { waitUntil: 'load' });
   await page.getByRole('textbox', { name: 'Email Address' }).click();
-  await page.getByRole('textbox', { name: 'Password' }).fill('Testing!123');
+  await page.getByRole('textbox', { name: 'Password' }).fill(password);
   await page.getByRole('button', { name: 'Sign In' }).click();
   await expect(page.getByText("Invalid Email or Password")).toBeVisible();
   });
@@ -44,9 +48,10 @@ test('Successful sign in', async ({ page }) => {
 
   //Invalid email format
   test('Invalid email format', async ({ page }) => {
+  const password = process.env.LOGIN_PASSWORD!;
   await page.goto('https://playground.testingart.com/', { waitUntil: 'load' });
   await page.getByRole('textbox', { name: 'Email Address' }).fill('testingartemail.com');
-  await page.getByRole('textbox', { name: 'Password' }).fill('Testing!123');
+  await page.getByRole('textbox', { name: 'Password' }).fill(password);
   await page.getByRole('button', { name: 'Sign In' }).click();
   await expect(page.getByText("Invalid Email or Password")).toBeVisible();
   });
@@ -54,8 +59,9 @@ test('Successful sign in', async ({ page }) => {
 
   //Valid email, wrong password
   test('Valid email, wrong password', async ({ page }) => {
+  const email = process.env.LOGIN_EMAIL!;
   await page.goto('https://playground.testingart.com/', { waitUntil: 'load' });
-  await page.getByRole('textbox', { name: 'Email Address' }).fill('testingart@email.com');
+  await page.getByRole('textbox', { name: 'Email Address' }).fill(email);
   await page.getByRole('textbox', { name: 'Password' }).fill('Testing!12');
   await page.getByRole('button', { name: 'Sign In' }).click();
   await expect(page.getByText("Invalid Email or Password")).toBeVisible();
@@ -64,18 +70,21 @@ test('Successful sign in', async ({ page }) => {
 
   //Special characters in email
   test('Special characters in email', async ({ page }) => {
+  const password = process.env.LOGIN_PASSWORD!;
   await page.goto('https://playground.testingart.com/', { waitUntil: 'load' });
   await page.getByRole('textbox', { name: 'Email Address' }).fill('testingart()@email.com');
-  await page.getByRole('textbox', { name: 'Password' }).fill('Testing!123');
+  await page.getByRole('textbox', { name: 'Password' }).fill(password);
   await page.getByRole('button', { name: 'Sign In' }).click();
   await expect(page.getByText("Invalid Email or Password")).toBeVisible();
   });
 
+  
   //Case sensitivity check for email address
   test('Case sensitivity check', async ({ page }) => {
+  const password = process.env.LOGIN_PASSWORD!;
   await page.goto('https://playground.testingart.com/', { waitUntil: 'load' });
   await page.getByRole('textbox', { name: 'Email Address' }).fill('Testingart@email.com');
-  await page.getByRole('textbox', { name: 'Password' }).fill('Testing!123');
+  await page.getByRole('textbox', { name: 'Password' }).fill(password);
   await page.getByRole('button', { name: 'Sign In' }).click();
   await expect(page.getByText("Logout")).toBeVisible();
   await page.getByText("Logout").click();
