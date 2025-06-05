c398702c55f8dbd27bc33a96c069f638010b41d8
SHasmikS <hasmik.suqiasyan@gmail.com>
2025-06-05 16:07:38 +0400

    Update homework3.spec.ts

--- tests/homework3.spec.ts
+++ tests/homework3.spec.ts
@@ -1,28 +1,32 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
-test('Find the product', async ({ page }) => {
-  await page.goto('https://www.zigzag.am/en/', { waitUntil: 'load' });
-  await page.locator('#search').fill('Samsung washing machine');
-  await page.locator('#search').press('Enter');
+test("Find the product", async ({ page }) => {
+  await page.goto("https://www.zigzag.am/en/", { waitUntil: "load" });
+  await page.locator("#search").fill("Samsung washing machine");
+  await page.locator("#search").press("Enter");
   await page.waitForSelector(".block_inner.product-item-details");
-  await page.locator('.block_inner.product-item-details')
-           .filter({ hasText: 'Samsung WW80AGAS21AXLP' })
-           .click();
-  });
+  await page
+    .locator(".block_inner.product-item-details")
+    .filter({ hasText: "Samsung WW80AGAS21AXLP" })
+    .click();
+});
 
+test("Compare the product", async ({ page }) => {
+  await page.goto("https://www.zigzag.am/en/", { waitUntil: "load" });
+  await page.locator("#search").fill("Samsung washing machine");
+  await page.locator("#search").press("Enter");
+  await page.waitForSelector(".block_inner.product-item-details");
 
-test('Compare the product', async ({ page }) => {
-  await page.goto('https://www.zigzag.am/en/', { waitUntil: 'load' });
-  await page.locator('#search').fill('Samsung washing machine');
-  await page.locator('#search').press('Enter');
-  await page.waitForSelector('.block_inner.product-item-details');
-
-  const productCard = page.locator('.block_inner.product-item-details')
-    .filter({ hasText: 'Samsung WW80AGAS21AXLP' });
+  const productCard = page
+    .locator(".block_inner.product-item-details")
+    .filter({ hasText: "Samsung WW80AGAS21AXLP" });
   await expect(productCard).toBeVisible();
-  const nameFromCard = await productCard.getByRole('link', { name: 'Samsung WW80AGAS21AXLP' }).first().innerText();
+  const nameFromCard = await productCard
+    .getByRole("link", { name: "Samsung WW80AGAS21AXLP" })
+    .first()
+    .innerText();
   await productCard.click();
-  await page.waitForLoadState('domcontentloaded');
+  await page.waitForLoadState("domcontentloaded");
   const nameOnDetailPage = page.locator('[data-dynamic="name"]');
   await expect(nameOnDetailPage).toContainText(nameFromCard);
 });
