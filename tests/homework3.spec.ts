9fa09a5cf514ed087d1d422df5ccdd06dc6c9882
SHasmikS <hasmik.suqiasyan@gmail.com>
2025-06-05 16:05:45 +0400

    Update homework3.spec.ts

--- tests/homework3.spec.ts
+++ tests/homework3.spec.ts
@@ -11,17 +11,25 @@ test('Find the product', async ({ page }) => {
   });
 
 
-test.only('Compare the product', async ({ page }) => {
+test('Compare the product1', async ({ page }) => {
   await page.goto('https://www.zigzag.am/en/', { waitUntil: 'load' });
   await page.locator('#search').fill('Samsung washing machine');
   await page.locator('#search').press('Enter');
   await page.waitForSelector('.block_inner.product-item-details');
+
   const productCard = page.locator('.block_inner.product-item-details')
     .filter({ hasText: 'Samsung WW80AGAS21AXLP' });
+
   await expect(productCard).toBeVisible();
-  const nameFromCard = await productCard.locator('xpath=//*[@id="product-item-info_42671"]/div/div[2]/a[1]').innerText();
+
+  // Use getByRole or getByText if visible text is enough
+  const nameFromCard = await productCard.getByRole('link', { name: 'Samsung WW80AGAS21AXLP' }).first().innerText();
+
   await productCard.click();
-  await page.waitForLoadState('load');
+
+  // Wait for navigation to the product detail page
+  await page.waitForLoadState('domcontentloaded');
+
   const nameOnDetailPage = page.locator('[data-dynamic="name"]');
   await expect(nameOnDetailPage).toContainText(nameFromCard);
 });
