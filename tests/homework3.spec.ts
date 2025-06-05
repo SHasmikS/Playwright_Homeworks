32c678554345dcd805b52b1be3ee69533070c401
SHasmikS <hasmik.suqiasyan@gmail.com>
2025-06-05 15:54:17 +0400

    Update homework3.spec.ts

--- tests/homework3.spec.ts
+++ tests/homework3.spec.ts
@@ -16,22 +16,12 @@ test.only('Compare the product', async ({ page }) => {
   await page.locator('#search').fill('Samsung washing machine');
   await page.locator('#search').press('Enter');
   await page.waitForSelector('.block_inner.product-item-details');
-
-  // Filter product card by text
   const productCard = page.locator('.block_inner.product-item-details')
     .filter({ hasText: 'Samsung WW80AGAS21AXLP' });
   await expect(productCard).toBeVisible();
-
-  // Get the product name text via XPath inside productCard
   const nameFromCard = await productCard.locator('xpath=//*[@id="product-item-info_42671"]/div/div[2]/a[1]').innerText();
-
-  // Click the product card to go to detail page
   await productCard.click();
-
-  // Wait for navigation or detail page to load
   await page.waitForLoadState('load');
-
-  // Locate the product name on detail page and assert it contains nameFromCard
   const nameOnDetailPage = page.locator('[data-dynamic="name"]');
   await expect(nameOnDetailPage).toContainText(nameFromCard);
 });
