b0b4705864ad1ae8c14dc31c5cdcc26c1b761322
SHasmikS <hasmik.suqiasyan@gmail.com>
2025-07-08 11:05:49 +0400

    1

---
+++ POM-6PM/basePage.ts
@@ -0,0 +1,21 @@
+import { Page, Locator, expect } from '@playwright/test';
+
+export class BasePage {
+  protected page: Page;
+
+  constructor(page: Page) {
+    this.page = page;
+  }
+
+  async type(locator: Locator, value: string) {
+    await locator.fill(value);
+  }
+
+  async click(locator: Locator) {
+    await locator.click();
+  }
+
+  async assertVisible(locator: Locator) {
+    await expect(locator).toBeVisible();
+  }
+}
---
+++ POM-6PM/searchResults6PM/searchResultsLocators.ts
@@ -0,0 +1,4 @@
+export const homePageLocators6PM = {
+pageHeader: 'h1.wu-z',
+searchIcon: 'button.D-z',
+};
